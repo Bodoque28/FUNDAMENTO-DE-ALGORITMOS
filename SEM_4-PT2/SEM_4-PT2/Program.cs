0023caccc3eede92add3f7d8cd0f7129da4fea1b
BODOQUE <epaytan4@gmail.com>
2025-09-09 11:17:03 -0500

    SEM_4

---
+++ SEM_4-PT2/SEM_4-PT2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM_4_PT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"EDUARDO\" ");
+        }
+        static void ejer1()
+        {
+            Console.WriteLine("NOMBRE: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("CARRERA: ");
+            string carrera = Console.ReadLine();
+            Console.WriteLine($"\n {nombre}  bienvenido a mundo UPN en su carrera de  {carrera}  :v");
+
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ SEM_4-PT2/SEM_4-PT2_PY/SEM_4_PT2_PY.py
@@ -0,0 +1,9 @@
+
+def ejer1():
+    nombre= input ("NOMBRE: ")
+    carrera = input("CARRERA: ")
+    print (f"\n {nombre}  bienvenido a mundo UPN en su carrera de  { carrera}  :v py")
+
+def ejer2():
+    print("\"EDUARDO :V\"")
+ejer2()
